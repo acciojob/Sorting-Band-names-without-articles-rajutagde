72375a0ec956c77328a8117e4d695c0603fad6ee
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-18 14:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-
-
-// suppose an array that contains a list of popular tourise spots
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-
-// So here the lexicographically sorted order is (without articles)---
-touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
-
-------------------------------++++++++++++++++++++++++++++++++++=---------------------------------------
------------------------------==============================-----------------------------------------
-
-//your code here
-	const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
-
-		function sortBands(bands) {
+function sortBands(bands) {
 			const articles = ['the', 'a', 'an'];
 			const sortedBands = bands.sort(function(a, b) {
 				const nameA = a.toLowerCase().replace(/^(the|an|a)\s+/, '');
