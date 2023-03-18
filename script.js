b90bac9ecfa11ca67fbfcb2f5e1dfcfb3267f130
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-18 14:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
+You are given an array of band names. You need to sort those band names in lexicographic order excluding any articles('a', 'an', 'the') and list down those names inside ul id='band' tag using the li tag.
+
+Ex:
+
+// suppose an array that contains a list of popular tourise spots
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+// So here the lexicographically sorted order is (without articles)---
+touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+
+------------------------------++++++++++++++++++++++++++++++++++=---------------------------------------
+-----------------------------==============================-----------------------------------------
+
+//your code here
+	const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
 
 		function sortBands(bands) {
 			const articles = ['the', 'a', 'an'];
